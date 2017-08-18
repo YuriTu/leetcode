a511dd82692a780cbc7a1ef9730478718025f02a
YuriTu <tuqiang_ru@163.com>
2017-08-18 10:47:09 +0800

    283. Move Zeroes.js

---
+++ 283. Move Zeroes.js
@@ -0,0 +1,18 @@
+/**
+ * 这个题不适合js，我觉得应该是有一次性添加0的方式的，应该考的是这一点
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let count = 0;
+    for (let i = 0, len = nums.length;i < len;i++){
+        if (nums[i] === 0){
+            nums.splice(i, 1)
+            count++;
+            i--;
+        }
+    }
+    for (let i = 0;i < count;i++){
+        nums.push(0);
+    }
+};
